31d5a782b4d6ddd0e54e5f5ea9ba979a07d44a87
nodvic <victor.mstrd@gmail.com>
2026-03-30 20:03:21 +0200

    state bucket

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "gcs" {
+    bucket = "innovatech-tf-state-491409"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
